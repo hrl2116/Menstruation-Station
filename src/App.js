ce8c8d1150f797772f8c6501fa52eb1077003432
Manasi S <manasinsoman@gmail.com>
2022-12-17 19:00:09 -0500

    fixed search and scroll bar

--- src/App.js
+++ src/App.js
@@ -6,25 +6,65 @@ import SearchBar from './SearchBar';
 import ListScroll from "./ListScroll";
 import React, { useRef, useEffect, useState } from 'react';
 
-const initialSongList = [
-  ]
+// const initialSongList = [
+//   ]
+
+// const buildings = [
+//   {name: 'Mudd'},
+//   {name: 'Schapiro'},
+//   {name: 'Hamilton'},
+//   {name: 'Butler Library'},
+//   {name: 'Lerner Hall'},
+//   {name: 'Uris'}
+// ];
+
+// const buttons = [
+//   {id: 1,label: 'Mudd'},
+//   {id: 2,label: 'Schapiro'},
+//   {id: 3,label: 'Hamilton'},
+//   {id: 4,label: 'Butler Library'},
+//   {id: 5,label: 'Lerner Hall'},
+//   {id: 6,label: 'Uris'}
+// ];
+
 
 function App() {
-  const [songList, setSongList] = useState(initialSongList);
+  // const [songList, setSongList] = useState(initialSongList);
 
-  const buttons = [
-    {label: 'Mudd'},
-    {label: 'Schapiro'},
-    {label: 'Hamilton'},
-    {label: 'Butler Library'},
-    {label: 'Lerner Hall'},
-    {label: 'Uris'}
-  ];
+  // const [label, setName] = useState('');
+
+  // const [foundUsers, setFoundUsers] = useState(buttons);
+
+  // const filter = (e) => {
+  //   const keyword = e.target.value;
+
+  //   if (keyword !== '') {
+  //     const results = buttons.filter((user) => {
+  //       return user.label.toLowerCase().startsWith(keyword.toLowerCase());
+  //       // Use the toLowerCase() method to make it case-insensitive
+  //     });
+  //     setFoundUsers(results);
+  //   } else {
+  //     setFoundUsers(buttons);
+  //     // If the text field is empty, show all users
+  //   }
+
+  //   setName(keyword);
+  // };
+
+  // const buttons = [
+  //   {label: 'Mudd'},
+  //   {label: 'Schapiro'},
+  //   {label: 'Hamilton'},
+  //   {label: 'Butler Library'},
+  //   {label: 'Lerner Hall'},
+  //   {label: 'Uris'}
+  // ];
 
 
-  useEffect(() => {
+  // useEffect(() => {
 
-  },[])
+  // },[])
 
   // function handleAdd(newItem) {
   //   const newList = [...songList, newItem];
@@ -45,11 +85,13 @@ function App() {
       <div className="Header">Menstruation Station</div>
       
       <Map />
-      <SearchBar />
-      <ListScroll buttons={buttons}/>
-      {/* <Form addItem={handleAdd}/> */}
-      {/* <List list={songList} removeItem={handleDelete}/> */}
+      {/* <SearchBar /> */}
+      <ListScroll />
+
+
     </div>
+
+
 
   
   );
--- src/ListScroll.js
+++ src/ListScroll.js
@@ -1,15 +1,108 @@
-import React from 'react';
+//import React from 'react';
 
-function ListScroll(props) {
-  const buttons = props.buttons;
+//import React from 'react';
+
+// const MyButton = ({ className }) => {
+//   return (
+//     <button className={className}>
+//       Click me!
+//     </button>
+//   );
+// };
+
+// const ListScroll = () => {
+//   return (
+//     <div className="scrollable-button-list">
+//       <MyButton className="wide-button" />
+//       <MyButton className="wide-button" />
+//       <MyButton className="wide-button" />
+//     </div>
+//   );
+// };
+
+// export default ListScroll;
+
+
+
+// function ListScroll(props) {
+//   const buttons = props.buttons;
+
+  // return (
+  //   <div classname = "scrollable-button-list" style={{overflow: 'scroll', height: '90px', display: 'flex', flexDirection: 'column'}}>
+  //     {buttons.map((button, index) => (
+  //       <button className="wide-button" key={index}>{button.label}</button>))}
+  //   </div>
+  // );
+// }
+
+// import React, { useState } from 'react';
+// import './ListScroll.css';
+
+// function ListScroll() {
+//   const [searchQuery, setSearchQuery] = useState('');
+//   const [buttons, setButtons] = useState([
+//     { id: 1, label: 'Mudd' },
+//     { id: 2, label: 'Schapiro' },
+//     { id: 3, label: 'Hamilton' },
+//     { id: 4, label: 'Butler Library' },
+//     { id: 5, label: 'Lerner' },
+//     { id: 6, label: 'Uris' },
+//     { id: 7, label: 'Lehman' },
+//     { id: 8, label: 'Noco' },
+//   ]);
+
+//   const handleSearch = (event) => {
+//     setSearchQuery(event.target.value);
+//   }
+
+//   const filteredButtons = buttons.filter((button) => button.label.toLowerCase().includes(searchQuery.toLowerCase()));
+
+//   return (
+//     <div className="scrollable-button-list" style={{overflow: 'scroll', height: '90px', display: 'flex', flexDirection: 'column'}}>
+//       <input type="text" placeholder="Search" value={searchQuery} onChange={handleSearch} />
+//       {filteredButtons.map((button) => (
+//         <button key={button.id}>{button.label}</button>
+//       ))}
+//     </div>
+//   );
+// }
+
+
+import React, { useState } from 'react';
+import './ListScroll.css';
+
+function ListScroll() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [buttons, setButtons] = useState([
+    { id: 1, label: 'Mudd' },
+    { id: 2, label: 'Schapiro' },
+    { id: 3, label: 'Hamilton' },
+    { id: 4, label: 'Butler Library' },
+    { id: 5, label: 'Lerner' },
+    { id: 6, label: 'Uris' },
+    { id: 7, label: 'Lehman' },
+    { id: 8, label: 'Noco' },
+  ]);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  }
+
+  const filteredButtons = buttons.filter((button) => button.label.toLowerCase().includes(searchQuery.toLowerCase()));
 
   return (
-    <div style={{overflow: 'scroll', height: '90px', display: 'flex', flexDirection: 'column'}}>
-      {buttons.map((button, index) => (
-        <button key={index}>{button.label}</button>
-      ))}
+    <div className="button-list">
+      <div>
+        <input type="text" placeholder="Search" value={searchQuery} onChange={handleSearch} />
+      </div>
+      <div className="scrolly" style={{overflow: 'scroll', height: '100px', width: '500px', display: 'flex', flexDirection: 'column'}}>
+        {filteredButtons.map((button) => (
+          <button className="wide-button" key={button.id}>{button.label}</button>
+        ))}
+      </div>
     </div>
   );
 }
+
 
 export default ListScroll;
